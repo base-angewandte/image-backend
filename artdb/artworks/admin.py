a2a8fbc3f04be73b5168c8e8732940d7c8031c24
Armin Wagner <noreply@arminbwagner.com>
2018-10-10 18:42:40 +0200

    using ArtworkForm in ArtworkAdmin (for django-autocomplete-light) using date calculation now in ArtworkAdmin

--- artdb/artworks/admin.py
+++ artdb/artworks/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
 from artworks.models import *
+from artworks.forms import ArtworkForm
 
-admin.site.register(Artwork)
+class ArtworkAdmin(admin.ModelAdmin):
+    form = ArtworkForm
+    class Media:
+        js = ['js/artwork_form.js']
+
 admin.site.register(ArtworkCollection)
 admin.site.register(Artist)
+admin.site.register(Artwork, ArtworkAdmin)
