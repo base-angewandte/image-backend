a7ecdfb3346699c0a0d4d461a298b7167881aaf0
Philipp Mayer <philipp.mayer@paradoxon.at>
2018-10-12 16:50:59 +0200

    + prepared handling of permissions

---
+++ artdb/general/signals.py
@@ -0,0 +1,24 @@
+from django.db.models import Q
+from django.contrib.auth.models import Permission
+from django.dispatch import receiver
+
+from django_cas_ng.signals import cas_user_authenticated
+
+
+@receiver(cas_user_authenticated, dispatch_uid="process_user_attributes")
+def process_user_attributes(sender, user, created, attributes, *args, **kwargs):
+    if not user or not attributes:
+        return
+
+    permissions = attributes.get('permissions')
+
+    # TODO
+    if permissions and 'TBD' in permissions.split(','):
+        user.is_staff = True
+        # p = Permission.objects.filter().exclude()
+        # user.user_permissions.set(p)
+    else:
+        user.is_staff = False
+        user.user_permissions.clear()
+
+    user.save()
