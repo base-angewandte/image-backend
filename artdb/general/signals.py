9bbbb1d30d7e838d3e6030d86a2cd36f1f321cce
Philipp Mayer <philipp.mayer@paradoxon.at>
2019-01-28 16:16:30 +0100

    ~ changed superuser handling

--- artdb/general/signals.py
+++ artdb/general/signals.py
@@ -23,8 +23,10 @@ def process_user_attributes(sender, user, created, attributes, *args, **kwargs):
         # user.user_permissions.clear()
 
     if user.username in settings.SUPERUSERS:
+        user.is_staff = True
         user.is_superuser = True
     else:
+        user.is_staff = False
         user.is_superuser = False
 
     user.save()
