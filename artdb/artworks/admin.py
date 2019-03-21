ac63189f0699e3a21a92f3ac585dc311104e0158
Philipp Mayer <philipp.mayer@paradoxon.at>
2019-03-21 12:07:00 +0100

    ~ pep8 changes

--- artdb/artworks/admin.py
+++ artdb/artworks/admin.py
@@ -6,6 +6,7 @@ from artworks.models import *
 from artworks.forms import ArtworkForm, ArtworkAdminForm
 from ordered_model.admin import OrderedTabularInline, OrderedInlineModelAdminMixin
 
+
 class ArtworkCollectionMembershipInline(OrderedTabularInline):
     model = ArtworkCollectionMembership
     autocomplete_fields = ['artwork']
@@ -35,11 +36,31 @@ class ArtworkAdmin(admin.ModelAdmin):
     list_display = ('title', 'checked', 'published', 'created_at', 'updated_at')
     ordering = ('-created_at',)
     search_fields = ['title']
-    fields = ('published', 'checked', 'thumbnail_image', 'image_original', 'title', 'title_english', 'artists', 'date', 'date_year_from', 'date_year_to', 'material', 'dimensions', 'keywords', 'location_of_creation', 'location_current', 'description', 'credits', 'created_at', 'updated_at')
-    readonly_fields = ('created_at','updated_at', 'thumbnail_image')
+    fields = (
+        'published',
+        'checked',
+        'thumbnail_image',
+        'image_original',
+        'title',
+        'title_english',
+        'artists',
+        'date',
+        'date_year_from',
+        'date_year_to',
+        'material',
+        'dimensions',
+        'keywords',
+        'location_of_creation',
+        'location_current',
+        'description',
+        'credits',
+        'created_at',
+        'updated_at',
+    )
+    readonly_fields = ('created_at', 'updated_at', 'thumbnail_image')
     formfield_overrides = {
-        models.CharField: {'widget': TextInput(attrs={'size':'80'})},
-        models.TextField: {'widget': Textarea(attrs={'rows':2, 'cols':80})},
+        models.CharField: {'widget': TextInput(attrs={'size': '80'})},
+        models.TextField: {'widget': Textarea(attrs={'rows': 2, 'cols': 80})},
     }
     list_filter = ('published', 'checked', CollectionListFilter, 'created_at', 'updated_at')
 
@@ -48,13 +69,13 @@ class ArtworkAdmin(admin.ModelAdmin):
 
     def thumbnail_image(self, obj):
         if obj.image_original:
-            return format_html('<img src="{url}" />'.format(url = obj.image_original.thumbnail['180x180']))
+            return format_html('<img src="{url}" />'.format(url=obj.image_original.thumbnail['180x180']))
         else:
             return format_html('none')
 
 
 class ArtworkCollectionAdmin(OrderedInlineModelAdminMixin, admin.ModelAdmin):
-    readonly_fields = ('created_at','updated_at')
+    readonly_fields = ('created_at', 'updated_at')
     list_display = ('title', 'created_at', 'updated_at')
     ordering = ('-created_at',)
     search_fields = ['title']
@@ -62,10 +83,10 @@ class ArtworkCollectionAdmin(OrderedInlineModelAdminMixin, admin.ModelAdmin):
 
 
 class ArtistAdmin(admin.ModelAdmin):
-    readonly_fields = ('created_at','updated_at')
+    readonly_fields = ('created_at', 'updated_at')
     list_display = ('name', 'created_at', 'updated_at')
     ordering = ('-created_at',)
-    search_fields = ['name',]
+    search_fields = ['name', ]
     list_filter = ('created_at', 'updated_at')
 
 
@@ -82,5 +103,3 @@ admin.site.register(Artist, ArtistAdmin)
 admin.site.register(Artwork, ArtworkAdmin)
 admin.site.register(Keyword, KeywordAdmin)
 admin.site.register(Location, LocationAdmin)
-
-
