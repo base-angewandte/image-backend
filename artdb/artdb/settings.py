8f9fc27ff4444ad1cd772ee23eb786a0d11abe4c
Armin Wagner <noreply@arminbwagner.com>
2019-04-05 13:01:40 +0200

    increased the rendition image quality (see Improvement #620)

--- artdb/artdb/settings.py
+++ artdb/artdb/settings.py
@@ -286,7 +286,7 @@ VERSATILEIMAGEFIELD_SETTINGS = {
     'cache_length': 2592000,
     # The save quality of modified JPEG images. More info here:
     # https://pillow.readthedocs.io/en/latest/handbook/image-file-formats.html#jpeg
-    'jpeg_resize_quality': 82,
+    'jpeg_resize_quality': 92,
     # Whether or not to create new images on-the-fly. Set this to `False` for
     # speedy performance but don't forget to 'pre-warm' to ensure they're
     # created and available at the appropriate URL.
