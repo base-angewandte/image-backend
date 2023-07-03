83fd109f203e095e03c04de8c0be4899076dae02
jackie / Andrea Ida Malkah Klaura <andrea.klaura@uni-ak.ac.at>
2023-07-03 14:17:23 +0200

    refactor: simplify migration 0041
    The initial migration before this fix was strangely convoluted and did
    not work if there was existing data for the folders
    (artworkcollectionmembership). Therefore it was reworked. But it turns
    out that Django's basic operations suffice just as well, so this is the
    migration Django initially should have created to rename the two models
    and add add an artworks manytomany field.

--- artdb/artworks/migrations/0041_auto_20230406_1511.py
+++ artdb/artworks/migrations/0041_auto_20230406_1511.py
@@ -1,32 +1,5 @@
 from django.conf import settings
 from django.db import migrations, models
-import django.db.models.deletion
-
-
-def copy_albums(apps, schema_editor):
-    """Copies all data from the artworkcollection table to the (new) album table"""
-    from django.db import connection
-    cursor = connection.cursor()
-    cursor.execute('INSERT INTO artworks_album SELECT * FROM artworks_artworkcollection')
-
-
-def copy_albums_reverse(apps, schema_editor):
-    """Deletes all rows on the album table"""
-    from django.db import connection
-    cursor = connection.cursor()
-    cursor.execute('TRUNCATE artworks_album')
-
-
-def copy_artworkcollection(apps, schema_editor):
-    """Copies (back) all data from the album table to the (old) artworkcollection table"""
-    from django.db import connection
-    cursor = connection.cursor()
-    cursor.execute('INSERT INTO artworks_artworkcollection SELECT * FROM artworks_album')
-
-
-def noop(apps, schema_editor):
-    """Placeholder for an empty operation"""
-    pass
 
 
 class Migration(migrations.Migration):
@@ -37,37 +10,13 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        migrations.CreateModel(
-            name='Album',
-            fields=[
-                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('title', models.CharField(max_length=255, verbose_name='Title')),
-                ('created_at', models.DateTimeField(auto_now_add=True, verbose_name='Created at')),
-                ('updated_at', models.DateTimeField(auto_now=True, verbose_name='Updated at')),
-                ('user', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL, verbose_name='User')),
-            ],
-            options={
-                'verbose_name': 'Folder',
-                'verbose_name_plural': 'Folders',
-            },
+        migrations.RenameModel(
+            old_name='ArtworkCollection',
+            new_name='Album'
         ),
-        migrations.RunPython(code=copy_albums, reverse_code=copy_albums_reverse),
-        migrations.AlterField(
-            model_name='artworkcollectionmembership',
-            name='collection',
-            field=models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='artworks.Album',
-                                    verbose_name='Folder'),
-        ),
-        # before the previous migration can be reversed, we need to copy back all album
-        # data into the artworkcollection table, otherwise the foreign key constraint will
-        # raise an error
-        migrations.RunPython(code=noop, reverse_code=copy_artworkcollection),
         migrations.RenameModel(
             old_name='ArtworkCollectionMembership',
-            new_name='AlbumMembership',
-        ),
-        migrations.DeleteModel(
-            name='ArtworkCollection',
+            new_name='AlbumMembership'
         ),
         migrations.AddField(
             model_name='album',
