625266ac47969bcc7630885825135212ea51b651
Marta Palandri <marta.palandri@uni-ak.ac.at>
2023-10-30 17:36:30 +0100

    feat(api): only allowed users who are authenticated (settings changes)

--- artdb/artdb/settings.py
+++ artdb/artdb/settings.py
@@ -122,6 +122,8 @@ INSTALLED_APPS = [
 
 REST_FRAMEWORK = {
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+    'DEFAULT_AUTHENTICATION_CLASSES': ('rest_framework.authentication.SessionAuthentication',),
+    'DEFAULT_PERMISSION_CLASSES': ('rest_framework.permissions.IsAuthenticated',),
 }
 
 SPECTACULAR_SETTINGS = {
