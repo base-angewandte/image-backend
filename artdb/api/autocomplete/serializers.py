74c870a2b2f5706787d2c3d975660f704fea543b
Philipp Mayer <philipp.mayer@paradoxon.at>
2024-02-06 11:32:00 +0100

    feat: add autocomplete serializer

---
+++ artdb/api/autocomplete/serializers.py
@@ -0,0 +1,24 @@
+from rest_framework import serializers
+
+SOURCES = [
+    'users',
+    'albums',
+    'title',
+    'artist',
+    'keywords',
+    'origin',
+    'location',
+    'permissions',
+]
+
+
+class AutocompleteRequestSerializer(serializers.Serializer):
+    limit = serializers.IntegerField(min_value=1, required=False, default=10)
+    q = serializers.CharField(required=True)
+    type = serializers.CharField(required=True)
+
+    def validate_type(self, value):
+        for t in value.split(','):
+            if t not in SOURCES:
+                raise serializers.ValidationError(f'{t} is not a valid type')
+        return value
