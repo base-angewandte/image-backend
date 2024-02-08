95fdd03f649d4a20b5299fa8361361c6e1dbbe6c
Philipp Mayer <philipp.mayer@paradoxon.at>
2024-02-08 12:03:19 +0100

    feat(api): set default parser and renderer to json

--- artdb/artdb/settings.py
+++ artdb/artdb/settings.py
@@ -429,7 +429,9 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'base_common_drf.authentication.SessionAuthentication',
     ),
+    'DEFAULT_PARSER_CLASSES': ('rest_framework.parsers.JSONParser',),
     'DEFAULT_PERMISSION_CLASSES': ('rest_framework.permissions.IsAuthenticated',),
+    'DEFAULT_RENDERER_CLASSES': ('rest_framework.renderers.JSONRenderer',),
     'DEFAULT_VERSIONING_CLASS': 'rest_framework.versioning.URLPathVersioning',
     'DEFAULT_VERSION': 'v1',
     'DEFAULT_SCHEMA_CLASS': 'base_common_drf.openapi.AutoSchema',
