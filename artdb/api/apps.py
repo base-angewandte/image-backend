dc6cd94d0a644bb8cbd629f3da57667ceb6c6bf9
Philipp Mayer <philipp.mayer@paradoxon.at>
2024-02-08 11:19:50 +0100

    fix: import SessionScheme

--- artdb/api/apps.py
+++ artdb/api/apps.py
@@ -3,3 +3,6 @@ from django.apps import AppConfig
 
 class ApiConfig(AppConfig):
     name = 'api'
+
+    def ready(self):
+        from base_common_drf.openapi.authentication import SessionScheme  # noqa: F401
