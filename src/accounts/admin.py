3cbb227324e33ebe6ee488538dd81723c724bff9
Philipp Mayer <philipp.mayer@paradoxon.at>
2024-07-15 12:25:12 +0200

    feat: add tos_accepted to user model

---
+++ src/accounts/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.utils.translation import gettext_lazy as _
+
+from .models import User
+
+
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    fieldsets = (
+        *UserAdmin.fieldsets,
+        (_('Additional info'), {'fields': ('tos_accepted',)}),
+    )
--- src/accounts/models.py
+++ src/accounts/models.py
@@ -1,6 +1,9 @@
 from django.contrib.auth.models import AbstractUser
+from django.db import models
 
 
 class User(AbstractUser):
+    tos_accepted = models.BooleanField(default=False)
+
     class Meta:
         db_table = 'auth_user'
