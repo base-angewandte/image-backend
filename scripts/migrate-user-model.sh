6b77655d74b40aa601ed247c39148412bef18489
Philipp Mayer <philipp.mayer@paradoxon.at>
2024-07-29 15:02:41 +0200

    change: use PROJECT_NAME in migrate-user-model.sh

--- scripts/migrate-user-model.sh
+++ scripts/migrate-user-model.sh
@@ -2,10 +2,10 @@
 
 set -e
 
-if docker compose exec image-django python manage.py showmigrations auth --list | grep -q "\[X\] 0001_initial" && docker compose exec image-django python manage.py showmigrations accounts --list | grep -q "\[ \] 0001_initial"; then
+if docker compose exec "${PROJECT_NAME}"-django python manage.py showmigrations auth --list | grep -q "\[X\] 0001_initial" && docker compose exec "${PROJECT_NAME}"-django python manage.py showmigrations accounts --list | grep -q "\[ \] 0001_initial"; then
 	CURRENT_BRANCH="$(git rev-parse --abbrev-ref HEAD)"
 	git checkout 97cf6749cebef7e84ef98df4875d0f2c265efc39
-	docker compose exec image-django python manage.py migrate --fake accounts 0001
+	docker compose exec "${PROJECT_NAME}"-django python manage.py migrate --fake accounts 0001
 	git checkout "$CURRENT_BRANCH"
-	docker compose exec image-django python manage.py migrate
+	docker compose exec "${PROJECT_NAME}"-django python manage.py migrate
 fi
