2c47b123e5bf4d44ad62137aaf2e6048a3bb6ec6
Boni Vircheva <boni.vircheva@uni-ak.ac.at>
2024-07-31 14:37:29 +0200

    feat: add post_tos_accepted endpoint to user view

--- src/api/views/user.py
+++ src/api/views/user.py
@@ -1,5 +1,6 @@
 from base_common_drf.openapi.responses import ERROR_RESPONSES
-from drf_spectacular.utils import extend_schema
+from drf_spectacular.types import OpenApiTypes
+from drf_spectacular.utils import OpenApiExample, OpenApiParameter, extend_schema
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
@@ -26,3 +27,49 @@ def get_user_data(request, *args, **kwargs):
         'tos_accepted': attributes.get('tos_accepted'),
     }
     return Response(ret, status=200)
+
+
+@extend_schema(
+    tags=['user'],
+    parameters=[
+        OpenApiParameter(
+            name='tos_accepted',
+            type=OpenApiTypes.BOOL,
+            location=OpenApiParameter.QUERY,
+            required=True,
+            description="The user's TOS accepted status",
+        ),
+    ],
+    responses={
+        200: OpenApiTypes.OBJECT,
+        400: ERROR_RESPONSES[400],
+    },
+    examples=[
+        OpenApiExample(
+            'Accept TOS',
+            description='Example for accepting the terms of service',
+            value={'tos_accepted': True},
+        ),
+        OpenApiExample(
+            'Decline TOS',
+            description='Example for declining the terms of service',
+            value={'tos_accepted': False},
+        ),
+    ],
+)
+@api_view(['POST'])
+def post_tos_accepted(request, *args, **kwargs):
+    tos_accepted = request.query_params.get('tos_accepted', None)
+    if tos_accepted is None:
+        return Response({'error': 'tos_accepted parameter is missing.'}, status=400)
+    try:
+        tos_accepted_bool = tos_accepted.lower() == 'true'
+    except AttributeError:
+        return Response(
+            {'error': "Invalid value for tos_accepted. Must be 'true' or 'false'."},
+            status=400,
+        )
+    attributes = request.session.get('attributes', {})
+    attributes['tos_accepted'] = tos_accepted_bool
+    request.session['attributes'] = attributes
+    return Response({'tos_accepted': tos_accepted_bool}, status=200)
