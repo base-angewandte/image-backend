ff65ffb0292b9d5ceb2e220cfc189f7bb275a2f9
Boni Vircheva <boni.vircheva@uni-ak.ac.at>
2024-08-08 13:31:18 +0200

    chore: change tos_accepted example of serializer to boolean field

--- src/api/serializers/user.py
+++ src/api/serializers/user.py
@@ -42,4 +42,4 @@ class UserDataSerializer(serializers.Serializer):
         child=serializers.CharField(),
         help_text='The permissions this user has.',
     )
-    tos_accepted = serializers.CharField(help_text="The user's tos accepted status")
+    tos_accepted = serializers.BooleanField(help_text="The user's tos accepted status")
