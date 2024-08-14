9d4ea53670f9a8b7ee9212790a4d6fdf30267075
jackie / Andrea Ida Malkah Klaura <andrea.klaura@uni-ak.ac.at>
2024-08-14 15:58:01 +0200

    change: implement TosAcceptedPermission based on reverse_lazy

--- src/api/permissions.py
+++ src/api/permissions.py
@@ -1,15 +1,23 @@
 from rest_framework import permissions
 
+from django.urls import reverse_lazy
+
 
 class TosAcceptedPermission(permissions.BasePermission):
-    """Only allows full API access if the ToS where accepted."""
+    """Only allows full API access if the ToS were accepted."""
+
+    def __init__(self):
+        super().__init__()
+        versions = ['v1']
+        view_names = ['api-root', 'user', 'tos-list', 'tos-accept']
+        self.allowed_paths = [
+            reverse_lazy(view_name, kwargs={'version': version})
+            for view_name in view_names
+            for version in versions
+        ]
 
     def has_permission(self, request, view):
         if request.user.tos_accepted:
             return True
 
-        # TODO: discuss in review whether this is the best way to check
-        #   we could also use an isinstance check on the view, and the action if it is a ViewSet
-        #   but this would require some imports and also a way to identified the WrappedAPIView of the get_user_data
-        endpoint = request.path.strip().split('/')[-2]
-        return endpoint in ['tos', 'user', 'v1']
+        return request.path in self.allowed_paths
