1770120434f9d213defcdb134b868fa1d05599a8
Philipp Mayer <philipp.mayer@paradoxon.at>
2024-08-16 17:36:35 +0200

    fix: use correct path for highlight.js

--- src/artworks/admin/utils.py
+++ src/artworks/admin/utils.py
@@ -6,8 +6,8 @@ from django.utils.html import format_html
 
 def external_metadata_html(external_metadata):
     value = json.dumps(external_metadata, indent=2, ensure_ascii=False)
-    style = static('highlight/styles/intellij-light.min.css')
-    js = static('highlight/highlight.min.js')
+    style = static('vendor/highlight/styles/intellij-light.min.css')
+    js = static('vendor/highlight/highlight.min.js')
 
     return format_html(
         '<pre style="max-height:300px"><code class="language-json">{}</code></pre>'
