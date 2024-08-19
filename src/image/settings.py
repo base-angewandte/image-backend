38f26e2584d7ece5467b085e77d525642bfbbf3f
Philipp Mayer <philipp.mayer@paradoxon.at>
2024-08-19 11:57:38 +0200

    change: set english as default language

--- src/image/settings.py
+++ src/image/settings.py
@@ -263,7 +263,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.0/topics/i18n/
 
-LANGUAGE_CODE = 'de'
+LANGUAGE_CODE = 'en'
 TIME_ZONE = 'Europe/Vienna'
 USE_I18N = True
 USE_TZ = True
