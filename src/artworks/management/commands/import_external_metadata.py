1ce26326ffb07fad546b3276ea3126bde9c52cde
Boni Vircheva <boni.vircheva@uni-ak.ac.at>
2024-09-10 10:31:44 +0300

    fix: add preferredName/preferredNameEntityForThePerson to synonyms for the importer

--- src/artworks/management/commands/import_external_metadata.py
+++ src/artworks/management/commands/import_external_metadata.py
@@ -9,7 +9,7 @@ from django.core.exceptions import ValidationError
 from django.core.management.base import BaseCommand, CommandError
 from django.db import IntegrityError
 
-from artworks.models import Keyword, Location, Person
+from artworks.models import Keyword, Location, Person, add_preferred_name_to_synonyms
 
 
 class Command(BaseCommand):
@@ -153,6 +153,21 @@ class Command(BaseCommand):
                 if instance.name != entry[0]:
                     instance.name = entry[0]
                     instance.gnd_overwrite = False
+                    if data_type == 'artist':
+                        if 'preferredNameEntityForThePerson' in data:
+                            preferred_name = instance.construct_individual_name(
+                                data['preferredNameEntityForThePerson'],
+                            )
+                            if instance.synonyms:
+                                synonyms_list = instance.synonyms.split(', ')
+                                synonyms_list[0] = preferred_name
+                                instance.synonyms = ', '.join(synonyms_list)
+                            else:
+                                instance.synonyms = preferred_name
+                        elif 'preferredName' in data:
+                            add_preferred_name_to_synonyms(instance, data)
+                    elif data_type == 'location' and 'preferredName' in data:
+                        add_preferred_name_to_synonyms(instance, data)
                     updated_without_name.append(entry)
                 else:
                     updated.append(entry)
