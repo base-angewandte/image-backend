44e1bc31e89ff9683ae7b4f9e37ada88ac7a7cef
jackie / Andrea Ida Malkah Klaura <andrea.klaura@uni-ak.ac.at>
2024-09-30 23:50:14 +0200

    refactor: move user view to ViewSet

--- src/api/views/user.py
+++ src/api/views/user.py
@@ -1,28 +1,29 @@
 from base_common_drf.openapi.responses import ERROR_RESPONSES
 from drf_spectacular.utils import extend_schema
-from rest_framework.decorators import api_view
+from rest_framework import viewsets
 from rest_framework.response import Response
 
 from api.serializers.user import UserDataSerializer
 
 
-@extend_schema(
-    tags=['user'],
-    responses={
-        200: UserDataSerializer,
-        401: ERROR_RESPONSES[401],
-    },
-)
-@api_view(['GET'])
-def get_user_data(request, *args, **kwargs):
-    attributes = request.session.get('attributes', {})
-    ret = {
-        'id': request.user.username,
-        'name': request.user.get_full_name(),
-        'email': request.user.email,
-        'showroom_id': attributes.get('showroom_id'),
-        'groups': attributes.get('groups'),
-        'permissions': attributes.get('permissions'),
-        'tos_accepted': request.user.tos_accepted,
-    }
-    return Response(ret, status=200)
+class UserViewSet(viewsets.GenericViewSet):
+    @extend_schema(
+        tags=['user'],
+        responses={
+            200: UserDataSerializer,
+            401: ERROR_RESPONSES[401],
+        },
+    )
+    def list(self, request, *args, **kwargs):
+        """Retrieve a user's data and preferences."""
+        attributes = request.session.get('attributes', {})
+        ret = {
+            'id': request.user.username,
+            'name': request.user.get_full_name(),
+            'email': request.user.email,
+            'showroom_id': attributes.get('showroom_id'),
+            'groups': attributes.get('groups'),
+            'permissions': attributes.get('permissions'),
+            'tos_accepted': request.user.tos_accepted,
+        }
+        return Response(ret, status=200)
