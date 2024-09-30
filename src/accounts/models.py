fe650b6882f39f505217d0eb2b1bb13f1113b0df
jackie / Andrea Ida Malkah Klaura <andrea.klaura@uni-ak.ac.at>
2024-09-30 23:51:51 +0200

    prepare: add display settings to User model

--- src/accounts/models.py
+++ src/accounts/models.py
@@ -3,7 +3,18 @@ from django.db import models
 
 
 class User(AbstractUser):
+    DISPLAY_IMAGES_CHOICES = [('crop', 'crop'), ('resize', 'resize')]
+    DISPLAY_FOLDERS_CHOICES = [('list', 'list'), ('grid', 'grid')]
+
     tos_accepted = models.BooleanField(default=False)
+    display_images = models.CharField(
+        choices=DISPLAY_IMAGES_CHOICES,
+        default=DISPLAY_IMAGES_CHOICES[0][0],
+    )
+    display_folders = models.CharField(
+        choices=DISPLAY_FOLDERS_CHOICES,
+        default=DISPLAY_FOLDERS_CHOICES[0][0],
+    )
 
     @property
     def full_name(self):
