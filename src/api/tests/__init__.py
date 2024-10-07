3daa46fcfc362e845d4e7ee1fa29ccdbfe0d057b
Boni Vircheva <boni.vircheva@uni-ak.ac.at>
2024-10-07 16:43:10 +0200

    test: add authentication tests, first draft

--- src/api/tests/__init__.py
+++ src/api/tests/__init__.py
@@ -10,6 +10,8 @@ from django.urls import reverse
 
 from artworks.models import Album, Artwork, Keyword, Location, Material, Person
 
+VERSION = 'v1'
+
 
 def temporary_image():  # from https://stackoverflow.com/a/67611074
     bts = BytesIO()
@@ -251,23 +253,58 @@ class APITestCase(RestFrameworkAPITestCase):
         # which should be created on demand.
 
 
-class BaseTestCase(APITestCase):
-    def setUp(self):
-        user = get_user_model()
-        self.user = user.objects.create_user(username='testuser', password='12345')  # noqa
-        self.url = reverse('artwork-list', kwargs={'version': 'v1'})
-        self.check_unauthorized_requests(self.url)
-
-    def check_unauthorized_requests(self, url):
+class AuthenticationTestCase(RestFrameworkAPITestCase):
+    def check_unauthorized_requests(self):
         """Helper to check that all methods return 401 when user is logged
         out."""
-        self.client.logout()
+        # NB! Don't forget the data!
+        urls = [
+            reverse('artwork-list', kwargs={'version': VERSION}),
+            reverse('artwork-detail', kwargs={'pk': 1, 'version': VERSION}),
+            reverse(
+                'artwork-image',
+                kwargs={
+                    'pk': 1,
+                    'height': 30,
+                    'width': 30,
+                    'method': 'crop',
+                    'version': VERSION,
+                },
+            ),
+            reverse('artwork-download', kwargs={'pk': 1, 'version': VERSION}),
+            reverse('artwork-labels', kwargs={'version': VERSION}),
+            # data = {'title': 'Test Album'} -> post. get:
+            reverse('album-list', kwargs={'version': VERSION}),
+            # data = {'title': 'Test Album'}
+            reverse('album-detail', kwargs={'pk': 1, 'version': VERSION}),
+            reverse(
+                'album-append-artwork',
+                kwargs={'pk': 1, 'version': VERSION},
+            ),
+            # Dont forget: test_albums_create_slides data = [[{'id': artwork1.pk}, {'id': artwork2.pk}], [{'id': artwork3.pk}]]
+            reverse('album-slides', kwargs={'pk': 1, 'version': VERSION}),
+            # Don't forget test_albums_create_permissions data = [{'user': f'{new_user.username}', 'permissions': [{'id': 'VIEW'}]}]
+            reverse('album-permissions', kwargs={'pk': 1, 'version': VERSION}),
+            reverse('album-download', kwargs={'pk': 1, 'version': VERSION}),
+            reverse('folder-list', kwargs={'version': VERSION}),
+            reverse('folder-detail', kwargs={'pk': 1, 'version': VERSION}),
+            reverse('permission-list', kwargs={'version': VERSION}),
+            reverse('search', kwargs={'version': VERSION}),
+            reverse('search-filters', kwargs={'version': VERSION}),
+            reverse('tos-list', kwargs={'version': VERSION}),
+            reverse('tos-accept', kwargs={'version': VERSION}),
+            reverse('user-list', kwargs={'version': VERSION}),
+            reverse('user-preferences', kwargs={'version': VERSION}),
+            reverse('autocomplete', kwargs={'version': VERSION}),
+        ]
+        # Research how to send requests to all endpoints and check if a person, who isn't even a user gets a 401!
         methods = ['get', 'post', 'put', 'patch', 'delete']
-        for method in methods:
-            client_method = getattr(self.client, method.lower(), None)
-            if client_method is not None:
-                if method in ['get', 'delete']:
-                    response = client_method(url, format='json')
-                elif method in ['post', 'patch', 'put']:
-                    response = client_method(url, {}, format='json')
-                self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+        for url in urls:
+            for method in methods:
+                client_method = getattr(self.client, method.lower(), None)
+                if client_method is not None:
+                    if method in ['get', 'delete']:
+                        response = client_method(url, format='json')
+                    elif method in ['post', 'patch', 'put']:
+                        response = client_method(url, {}, format='json')
+                    self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
