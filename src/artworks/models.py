0ecac105d39d5bd77fb7b16708321fff07617728
Philipp Mayer <philipp.mayer@paradoxon.at>
2024-10-18 17:20:48 +0200

    refactor: refactor Location __str__

--- src/artworks/models.py
+++ src/artworks/models.py
@@ -400,13 +400,9 @@ class Location(MPTTModel, MetaDataMixin):
         order_insertion_by = ['name']
 
     def __str__(self):
-        try:
-            ancestors = self.get_ancestors(include_self=True)
-            ancestors = [i.name for i in ancestors]
-        except Exception:  # TODO: this should be more specific
-            ancestors = [self.name]
-
-        return ' > '.join(ancestors[: len(ancestors) + 1])
+        return ' > '.join(
+            self.get_ancestors(include_self=True).values_list('name', flat=True),
+        )
 
     def clean(self):
         super().clean()
