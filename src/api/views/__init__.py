b52b826111e11048eef9783f7ec1de44fdd1e5e1
Philipp Mayer <philipp.mayer@paradoxon.at>
2024-11-19 11:23:29 +0100

    docs: add comment

--- src/api/views/__init__.py
+++ src/api/views/__init__.py
@@ -132,7 +132,11 @@ def featured_artworks(album, request, num_artworks=4):
             else None,
             'title': artwork.title,
             'discriminatory_terms': [
-                dt.term for dt in artwork.discriminatory_terms.all()
+                # we iterate over discriminatory_terms directly instead of using
+                # artwork.get_discriminatory_terms_list() to ensure that we are
+                # using the results already fetched with prefetch_related()
+                dt.term
+                for dt in artwork.discriminatory_terms.all()
             ],
         }
 
