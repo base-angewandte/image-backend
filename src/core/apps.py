5e21ef0d3c2cd15f739ccb592ed1d64eda82ef80
Philipp Mayer <philipp.mayer@paradoxon.at>
2024-12-02 12:22:09 +0100

    change: move process_user_attributes signal handler to accounts

--- src/core/apps.py
+++ src/core/apps.py
@@ -4,7 +4,3 @@ from django.apps import AppConfig
 class CoreConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'core'
-
-    def ready(self):
-        # import signal handlers
-        from . import signals  # noqa: F401
