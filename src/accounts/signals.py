3cfb7a2116f94ef3a588e96ec7a97b307e1366e1
Philipp Mayer <philipp.mayer@paradoxon.at>
2024-12-02 12:36:04 +0100

    change: adapt signal to ensure group with correct permissions exist

--- src/accounts/signals.py
+++ src/accounts/signals.py
@@ -1,6 +1,7 @@
 from django_cas_ng.signals import cas_user_authenticated
 
-from django.contrib.auth.models import Group
+from django.conf import settings
+from django.contrib.auth.models import Group, Permission
 from django.dispatch import receiver
 
 
@@ -15,14 +16,36 @@ def process_user_attributes(sender, user, created, attributes, *args, **kwargs):
     user.is_staff = False
     user.is_superuser = False
 
-    if 'administer_image' in permissions:
-        user.is_staff = True
-        user.is_superuser = True
-
-    if 'edit_image' in permissions:
+    if 'administer_image' in permissions or 'edit_image' in permissions:
         user.is_staff = True
 
-        editor_group = Group.objects.get(name='editor')
+        editor_group, created = Group.objects.get_or_create(name=settings.EDITOR_GROUP)
+
+        if created:
+            app_labels = [
+                'artworks',
+                'texts',
+            ]
+            codenames = [
+                p
+                for m in (
+                    'artwork',
+                    'discriminatoryterm',
+                    'keyword',
+                    'location',
+                    'material',
+                    'person',
+                )
+                for p in (f'view_{m}', f'add_{m}', f'change_{m}', f'delete_{m}')
+            ] + ['change_text']
+
+            group_permissions = Permission.objects.filter(
+                content_type__app_label__in=app_labels,
+                codename__in=codenames,
+            )
+
+            editor_group.permissions.set(group_permissions)
+
         user.groups.add(editor_group)
     else:
         user.groups.clear()
