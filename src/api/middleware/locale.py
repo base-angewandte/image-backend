b68d54e3d3027cb9c33947d19086d9053afb1d0b
Boni Vircheva <boni.vircheva@uni-ak.ac.at>
2025-02-06 11:53:02 +0100

    change: change code logic, restructure api_prefix

--- src/api/middleware/locale.py
+++ src/api/middleware/locale.py
@@ -1,32 +1,42 @@
 from django.conf import settings
-from django.utils.translation import activate
 
 
 class LocaleMiddleware:
     def __init__(self, get_response):
         self.get_response = get_response
-        self.api_prefix = settings.API_PREFIX
+
+        if settings.FORCE_SCRIPT_NAME:
+            self.api_prefix = f'{settings.FORCE_SCRIPT_NAME}/{settings.API_PREFIX}'
+        else:
+            self.api_prefix = f'/{settings.API_PREFIX}'
 
     def __call__(self, request):
+        cookie_change = None
+
         if request.path.startswith(self.api_prefix):
-            accept_language = request.headers.get('accept-language')
+            current_set_cookie = request.COOKIES.get(settings.LANGUAGE_COOKIE_NAME)
 
-            language_code = (
-                accept_language.split(',')[0].split('-')[0].lower()
-                if accept_language
-                else settings.LANGUAGE_CODE
-            )
+            if current_set_cookie:
+                accept_language_header = request.headers.get('accept-language')
+                language_code = (
+                    accept_language_header
+                    if accept_language_header
+                    else settings.LANGUAGE_CODE
+                )
 
-            if language_code in settings.LANGUAGES_DICT:
-                activate(language_code)
-                request.LANGUAGE_CODE = language_code
+                if (
+                    accept_language_header in settings.LANGUAGES_DICT
+                    and current_set_cookie != accept_language_header
+                ):
+                    request.COOKIES[settings.LANGUAGE_COOKIE_NAME] = language_code
+                    cookie_change = language_code
 
         response = self.get_response(request)
 
-        if request.path.startswith(self.api_prefix):
+        if cookie_change:
             response.set_cookie(
                 settings.LANGUAGE_COOKIE_NAME,
-                request.LANGUAGE_CODE,
+                cookie_change,
                 max_age=settings.LANGUAGE_COOKIE_AGE,
             )
 
