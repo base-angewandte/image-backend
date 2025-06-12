e8780aba160d90bce1a68b6772781d085704bfb0
Boni Vircheva <boni.vircheva@uni-ak.ac.at>
2025-06-12 08:12:15 +0200

    chore: add comment explaining change

--- src/api/views/search.py
+++ src/api/views/search.py
@@ -331,6 +331,7 @@ class SearchViewSet(viewsets.GenericViewSet):
                 artwork_serialized['editing_link'] = artwork.editing_link
             results.append(artwork_serialized)
 
+        # Ensure total reflects the full number of artworks, even if the offset exceeds the available results
         if total == 0 and offset > 0:
             total = subq.count()
 
