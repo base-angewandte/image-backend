c60f45307169acc8ad404125efde1cf9c1769322
Boni Vircheva <boni.vircheva@uni-ak.ac.at>
2025-12-16 09:53:32 +0100

    feat: add extraction of media url to filepath

--- src/artworks/utils.py
+++ src/artworks/utils.py
@@ -1,4 +1,17 @@
+from django.conf import settings
+
+
 def remove_non_printable_characters(value: str):
     if value and not value.isprintable():
         return ''.join(ch for ch in value if ch.isprintable())
     return value
+
+
+def media_url_to_file_path(instance, url: str):
+    """Derive local filesystem path from a response URL."""
+
+    client_hostname = instance.client._base_environ()['SERVER_NAME']
+    media_path = url.split(
+        f'{client_hostname}{settings.MEDIA_URL}',
+    )[1]
+    return f'{settings.MEDIA_ROOT}/{media_path}'
