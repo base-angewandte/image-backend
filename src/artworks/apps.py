a727cbd68ba096cf5f248410d06c7cbf545e9a62
Boni Vircheva <boni.vircheva@uni-ak.ac.at>
2025-12-16 11:24:38 +0100

    remove: delete redundant monkeypatch

--- src/artworks/apps.py
+++ src/artworks/apps.py
@@ -1,32 +1,6 @@
-from versatileimagefield.datastructures import sizedimage
-
 from django.apps import AppConfig
-from django.conf import settings
 from django.db.models.signals import post_migrate
 from django.utils.translation import gettext_lazy as _
-
-
-def monkeypatch_versatile_image_field():
-    """
-    To bypass
-    https://github.com/respondcreate/django-versatileimagefield/issues/59
-    we monkey patch versatile image field to silent error when
-    a specific setting is toggled
-    """
-
-    def dec(func):
-        def wrapper(self, *args, **kwargs):
-            try:
-                return func(self, *args, **kwargs)
-            except FileNotFoundError:
-                if settings.DEBUG:
-                    return
-                raise
-
-        return wrapper
-
-    kls = sizedimage.SizedImage
-    kls.create_resized_image = dec(kls.create_resized_image)
 
 
 class ArtworksConfig(AppConfig):
@@ -38,6 +12,3 @@ class ArtworksConfig(AppConfig):
         from . import signals
 
         post_migrate.connect(signals.post_migrate_signal, sender=self)
-
-        if settings.DEBUG:
-            monkeypatch_versatile_image_field()
